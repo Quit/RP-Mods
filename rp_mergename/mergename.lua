891047a17324c24fc33a119f9906481d81f88a50
Zaubermuffin <zaubermuffin@gmail.com>
2014-01-07 12:40:07 +0100

    Fixed rp_mergename crashing.
    Seems like my poc snippet somehow slipped in. I've added (commented out) code for special naming (assuming two name parts) and "special pools" (i.e. a list of names that does not follow the usual "first name - last name" order). The JSON is not compatible to what I've posted on discourse, but the changes are minimal.

--- rp_mergename/mergename.lua
+++ rp_mergename/mergename.lua
@@ -1,13 +1,29 @@
-	local faction = api.population:get_faction("civ", "stonehearth:factions:ascendancy")
+local faction = api.population:get_faction("civ", "stonehearth:factions:ascendancy")
 
-	local function getRandom(names)
-		return names[math.random(#names)]
-	end
+local function getRandom(names)
+	return names[math.random(#names)]
+end
 
-	function faction:generate_random_name(gender)
-		local names = self._data[gender]
+local function getRandomName(names)
+	local firstName = getRandom(names)
+	local secondName
+	repeat
+		secondName = getRandom(names)
+	until secondName ~= firstName
+	
+	return firstName .. secondName:lower()
+end
 
-		local surnames = self._data.surnames
-		
-		return getRandomName(names.first_name_1) .. getRandom(names.first_name_2) .. ' ' .. getRandom(surnames.first_part) .. getRandom(surnames.last_part)
-	end
+function faction:generate_random_name(gender)
+	local names = self._data.given_names[gender]
+	local surnames = self._data.surnames
+	
+	-- Re: "Could we have special names that do not follow the normal naming?"
+--~ 	if math.random(#names + 1) == 1 then -- or any chance
+--~ 		return getRandom(self._data.special_names)
+--~ 	end
+	
+	-- If we had another format (as described on discourse), use this one.
+--~ 		return getRandom(names.first_name_1) .. getRandom(names.first_name_2) .. ' ' .. getRandom(surnames.first_part) .. getRandom(surnames.last_part)
+	return getRandomName(names) .. ' ' .. getRandomName(surnames)
+end
