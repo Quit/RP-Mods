8e32e8de3e6ef25b6e3aa1eec8552ae0b48e0861
RepeatPan <zaubermuffin+repeatpan@gmail.com>
2014-01-09 15:58:22 +0100

    Simplified file structure.
    While respecting the general file structure that Radiant uses is okay and all, I don't think five-file-mods should be required to do the same.

--- rp_workforce/server_init.lua
+++ rp_workforce/server_init.lua
@@ -2,7 +2,7 @@
 rp.add_entity_created_hook('stonehearth:camp_standard', function(self)
 	-- And add our little command.
 	local commands = self:add_component("stonehearth:commands")
-	commands:add_command("/rp_workforce/data/commands/summon_worker")
+	commands:add_command("/rp_workforce/commands/summon_worker")
 end)
 
 return true
