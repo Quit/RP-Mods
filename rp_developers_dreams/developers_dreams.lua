f4ec6804b819c9e6a9ea9205bd3486bcac315e18
RepeatPan <zaubermuffin+repeatpan@gmail.com>
2014-01-11 16:39:37 +0100

    Added size option.

--- rp_developers_dreams/developers_dreams.lua
+++ rp_developers_dreams/developers_dreams.lua
@@ -3,13 +3,17 @@ local WG = rp.load_stonehearth_service('world_generation.world_generator')
 
 local oldInit = WG.__user_init
 
+local size = math.max(rp.get_config('size', 1), 1)
+rp.logf('Size: %d', size)
+
 function WG:__init(async, seed)
 	return oldInit(self, async, 0) -- seed is always zero.
 end
 
 -- It's a tiny tiny - how often did I say this already
 function WG:_create_world_blueprint()
-	return self:_get_empty_blueprint(1, 1)
+	print('create blueprint of size ', size)
+	return self:_get_empty_blueprint(size, size)
 end
 
 return true
--- rp_test_world/test_world.lua
+++ rp_test_world/test_world.lua
@@ -2,12 +2,16 @@
 -- aka "a very small tile".
 local WG = rp.load_stonehearth_service('world_generation.world_generator')
 
+local size = math.max(rp.get_config('size', 1), 1)
+
+rp.logf('Size: %d', size)
+
 if not WG then
 	error('Could not load WorldGeneration service!')
 end
 
 function WG:_create_world_blueprint()
-	return self:_get_empty_blueprint(1, 1)
+	return self:_get_empty_blueprint(size, size)
 end
 
 return true
