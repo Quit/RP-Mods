23549968af0d2d2efe61609757a55ef1eaf370db
RepeatPan <zaubermuffin+repeatpan@gmail.com>
2014-02-07 19:00:23 +0100

    Updated for new RP messages.

--- rp_test_world/test_world.lua
+++ rp_test_world/test_world.lua
@@ -6,8 +6,8 @@ local WGS = radiant.mods.load('stonehearth').world_generation
 local function generator_chosen(_, event)
 	-- TODO: Have some sort of... proper event here?
 	-- But we don't intend on doing anything with this for a while. :(
-	event.generator._create_world_blueprint = function(self) return self:_get_empty_blueprint(size, size) end
+	event.generator.generate_blueprint = function(self) return self:get_empty_blueprint(size, size) end
 end
 
-radiant.events.listen(WGS, 'stonehearth:world_generator_chosen', WGS, generator_chosen)
+radiant.events.listen(WGS, 'rp:blueprint_generator_chosen', WGS, generator_chosen)
 return true
