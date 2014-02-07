43e2ce0c1d0d9661bb22824f29b58252d6c8e997
RepeatPan <zaubermuffin+repeatpan@gmail.com>
2014-02-07 20:58:24 +0100

    Fixed rp_developers_dreams.

--- rp_developers_dreams/developers_dreams.lua
+++ rp_developers_dreams/developers_dreams.lua
@@ -1,19 +1,16 @@
 local size = math.max(rp.get_config('size', 1), 1)
 
 -- Let's talk about world generation.
-local WG = rp.load_stonehearth_service('world_generation.world_generator')
-
 local WGS = radiant.mods.load('stonehearth').world_generation
 
-
-local function propose_generator(_, event)
-	local wg = WG(event.async, 0)
-	
-	wg._create_world_blueprint = function(self) return self:_get_empty_blueprint(size, size) end
-	
-	table.insert(event.proposals, { priority = 100, generator = wg })
+local function on_initialisation(_, event)
+	event.game_seed = 0
 end
 
-radiant.events.listen(WGS, 'stonehearth:propose_world_generator', WGS, propose_generator)
+local function generator_chosen(_, event)
+	event.generator.generate_blueprint = function(self) return self:get_empty_blueprint(size, size) end
+end
 
+radiant.events.listen(WGS, 'rp:blueprint_generator_chosen', WGS, generator_chosen)
+radiant.events.listen(WGS, 'rp:on_initialisation', WGS, on_initialisation)
 return true
