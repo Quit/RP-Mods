a4aafaa09758cd7b3d2b7e928e61c3b20e3eb3ec
RepeatPan <repeatpan@server.fake>
2014-02-11 04:58:55 +0100

    Use mixins to reduce script dependency
    Three things still untidy about this: 1) to disable harvest_shoot it's still necessary to have a script (it seems) 2) the command ordering is wrong 3) we should use the tree mixins if that one's working properly now.

--- rp_diy_trees/server_init.lua
+++ rp_diy_trees/server_init.lua
@@ -7,14 +7,14 @@ end
 function MOD:_on_entity_created(event)
 	if event.entity_id:find('oak_tree') and not event.entity:get_component('stonehearth:placeable_item_proxy') then
 		local commands = event.entity:add_component('stonehearth:commands')
-		commands:add_command('/rp_diy_trees/harvest_shoot')
+--~ 		commands:add_command('/rp_diy_trees/harvest_shoot')
 
-		event.entity:add_component('stonehearth:renewable_resource_node'):extend(
-		{
-			resource = "rp_diy_trees:growing_oak_tree_sapling",
-			renewal_time = "12h",
-			harvest_command = "harvest_shoot"
-		})
+--~ 		event.entity:add_component('stonehearth:renewable_resource_node'):extend(
+--~ 		{
+--~ 			resource = "rp_diy_trees:growing_oak_tree_sapling",
+--~ 			renewal_time = "0",
+--~ 			harvest_command = "harvest_shoot"
+--~ 		})
 		
 		if event.entity:get_component('rp_diy_trees:growable') then
 			commands:enable_command('harvest_shoot', false)
