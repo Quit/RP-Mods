ae3a9ac2f63c77a09a9c3792d4ecc7e86a77707d
RepeatPan <repeatpan@server.fake>
2014-02-14 02:03:11 +0100

    Updated both to r4099

--- rp_developers_dreams/developers_dreams.lua
+++ rp_developers_dreams/developers_dreams.lua
@@ -1,16 +1,19 @@
-local size = math.max(rp.get_config('size', 1), 1)
+local size = math.max(rp.get_config('size', 2), 2)
 
 -- Let's talk about world generation.
 local WGS = radiant.mods.load('stonehearth').world_generation
 
-local function on_initialisation(_, event)
-	event.game_seed = 0
+local function set_seed(event)
+	event.seed = 0
 end
 
-local function generator_chosen(_, event)
-	event.generator.generate_blueprint = function(self) return self:get_empty_blueprint(size, size) end
+local function generator_chosen(event)
+	local get_empty = event.generator.get_empty_blueprint
+	
+	event.generator.generate_blueprint = function(self) return get_empty(self, size, size) end
+	event.generator.get_empty_blueprint = event.generator.generate_blueprint
 end
 
-radiant.events.listen(WGS, 'rp:blueprint_generator_chosen', WGS, generator_chosen)
-radiant.events.listen(WGS, 'rp:on_initialisation', WGS, on_initialisation)
+radiant.events.listen(WGS, 'rp:blueprint_generator_chosen', generator_chosen)
+radiant.events.listen(WGS, 'rp:set_seed', set_seed)
 return true
--- rp_test_world/test_world.lua
+++ rp_test_world/test_world.lua
@@ -1,13 +1,14 @@
-local size = math.max(rp.get_config('size', 1), 1)
+local size = math.max(rp.get_config('size', 2), 2)
 
 -- Let's talk about world generation.
 local WGS = radiant.mods.load('stonehearth').world_generation
 
-local function generator_chosen(_, event)
-	-- TODO: Have some sort of... proper event here?
-	-- But we don't intend on doing anything with this for a while. :(
-	event.generator.generate_blueprint = function(self) return self:get_empty_blueprint(size, size) end
+local function generator_chosen(event)
+	local get_empty = event.generator.get_empty_blueprint
+	
+	event.generator.generate_blueprint = function(self) return get_empty(self, size, size) end
+	event.generator.get_empty_blueprint = event.generator.generate_blueprint
 end
 
-radiant.events.listen(WGS, 'rp:blueprint_generator_chosen', WGS, generator_chosen)
+radiant.events.listen(WGS, 'rp:blueprint_generator_chosen', generator_chosen)
 return true
