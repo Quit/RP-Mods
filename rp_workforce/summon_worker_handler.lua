c7e75961886a0197446c3ffadfe6a7e831184f7c
RepeatPan <repeatpan@server.fake>
2014-05-05 22:30:27 +0200

    Updated to r80

--- rp_workforce/summon_worker_handler.lua
+++ rp_workforce/summon_worker_handler.lua
@@ -4,13 +4,12 @@ local api = radiant.mods.load('stonehearth')
 local personality_service = api.personality_service
 
 -- Copied straight from NewGameCallbackHandler.
-local function place_citizen(x, z)
-  local pop_service = api.population
-  local faction = pop_service:get_faction("civ", "stonehearth:factions:ascendancy")
-  local citizen = faction:create_new_citizen()
-  faction:promote_citizen(citizen, "worker")
+local function place_citizen(pop, x, z)
+	local citizen = pop:create_new_citizen()
+  local profession = "stonehearth:professions:worker"
+  pop:promote_citizen(citizen, profession)
   radiant.terrain.place_entity(citizen, Point3(x, 1, z))
-	
+  
 	-- Since we are summoning them, I am not entirely sure /how/ well that works
 	radiant.events.trigger(personality_service, "stonehearth:journal_event", {
     entity = citizen,
@@ -22,7 +21,7 @@ function SummonWorkerHandler:summon_worker(session, response, standard)
 	local pos = radiant.entities.get_location_aligned(standard)
 	pos.x = pos.x + math.random(-4, 4)
 	pos.z = pos.z + math.random(-4, 4)
-	rp.run_safe(place_citizen, pos.x, pos.z)
+	rp.run_safe(place_citizen, stonehearth.population:get_population(session.player_id), pos.x, pos.z)
 end
 
 return SummonWorkerHandler
